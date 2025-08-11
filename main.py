e4f8dc0be27116a52f9196253dff38517246d0c7
Luis Ponce <poncenavarroluisfelipe@gmail.com>
2025-08-11 10:29:30 -0600

    replace in-memory by json

--- main.py
+++ main.py
@@ -1,50 +1,113 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
+from typing import Optional
+import json
+import os
 import uvicorn
 
+JSON_FILENAME = "phonebook.json"
+
+# Use json as database:
+# https://dev.to/ahmed__elboshi/learn-how-to-use-json-as-a-small-database-for-your-python-projects-by-building-a-hotel-accounting-system-47b4
+
 app = FastAPI()
-
-items = []
-
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    return {"Phonebook App": "Hello! This is a simple phonebook app using FastAPI and a JSON file as a database."}
 
 # CRUD -> Create
-@app.post("/items")
-def create_item(item: str):
-    items.append(item)
-    return {"items": items}
+@app.post("/users")
+def create_user(name: str, last_name: str, phone: int, email: str, address: str):
+    # example:
+    # http://127.0.0.1:8000/users?name=%22Luis%22&last_name=%22Ponce%22&phone=33112233&email=%22test%40example.com%22&address=%22myaddress%22
+    try:
+        with open(JSON_FILENAME, 'r') as file:
+            data = json.load(file)
+    except:
+        data = {}
+    user = {
+        "name": name,
+        "last_name": last_name,
+        "phone": phone,
+        "email": email,
+        "address": address
+    }
+    fn = f"{name.lower()} {last_name.lower()}"
+    data[fn] = user
+    with open(JSON_FILENAME, 'w') as file:
+        json.dump(data, file, indent=4)
+    return user
 
 # CRUD -> Read
-@app.get("/items")
-def read_items():
-    return {"items": items}
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
-    if 0 <= item_id < len(items):
-        return items[item_id]
+@app.get("/users")
+def read_users():
+    if os.path.isfile(JSON_FILENAME) and os.access(JSON_FILENAME, os.R_OK):
+        with open(JSON_FILENAME, "r") as file:
+            try:
+                data = json.load(file)
+            except:
+                data = {}
     else:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail="read_users: Phonebook file not found or not readable")
+    return data
+
+@app.get("/users/{user_name}")
+# siempre tiene prioridad el PATH, y luego la query string
+def get_user(user_name: str, phone: Optional[int] = None) -> dict:
+    user_name = user_name.lower()
+    with open(JSON_FILENAME, "r") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            raise HTTPException(status_code=404, detail="get_user: Could not read read_users phonebook")
+    if phone != None:
+        for u in data.values():
+            if u["phone"] == phone:
+                return u
+    else:
+        for k in data.keys():
+            if k == user_name:
+                return data[k]
+    raise HTTPException(status_code=404, detail="User not found")
 
 # CRUD -> Update
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: str):
-    if 0 <= item_id < len(items):
-        items[item_id] = item
-        return {"item": items[item_id]}
-    else:
-        raise HTTPException(status_code=404, detail="Item not found to update")
+@app.put("/users/{user_name}")
+def update_user(user_name: str, phone: int, email: str, address: str):
+    user_name = user_name.lower()
+    with open(JSON_FILENAME, "r") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            raise HTTPException(status_code=404, detail="update_user: Could not read phonebook")
+    for k in data.keys():
+        if k == user_name:
+            user = data[k]
+            user["phone"] = phone
+            user["email"] = email
+            user["address"] = address
+            data[k] = user
+            with open(JSON_FILENAME, "w") as file:
+                json.dump(data, file)
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
 
 # CRUD -> Delete
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    if 0 <= item_id < len(items):
-        deleted_item = items.pop(item_id)
-        return {"deleted_item": deleted_item}
-    else:
-        raise HTTPException(status_code=404, detail="Item not found to delete")
-
+@app.delete("/users/{user_name}")
+def delete_user(user_name: str):
+    user_name = user_name.lower()
+    with open(JSON_FILENAME, "r") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            raise HTTPException(status_code=404, detail="delete_user: Could not read phonebook")
+    
+    for k in data.keys():
+        if k == user_name:
+            user = data[k]
+            del data[k]
+            with open(JSON_FILENAME, "w") as file:
+                json.dump(data, file)
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
